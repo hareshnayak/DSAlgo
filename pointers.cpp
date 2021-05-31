9f3a65f65da70fbef3407d934104765e8913cba2
hareshnayak <sketchharry01@gmail.com>
2021-06-01 00:40:08 +0530

    multiple pointers

--- pointers.cpp
+++ pointers.cpp
@@ -8,13 +8,39 @@ int main(){
 
     // doesn't work for character variables
     char ch = 'S';
-    cout<<&ch;
+    char* chptr = &ch;
+    cout<<chptr<<endl;
 
     // to print the address of the character
     // type caste it from char* to void* or int*
-    cout<<(void*)&ch<<endl;
+    cout<<(void*)chptr<<endl;
+
+    /* Pointer variable
+    Pointer variable stores the address of a variable
+    */
+    int* ptr;
+    ptr = & x; // ptr stores address of x
+
+    float a = 10.7;
+    float* aptr = &a;
 
 
+    cout<<x<<" "<<ptr<<endl;
+    cout<<a<<" "<<aptr<<endl;
+    cout<<ch<<" "<<chptr<<endl;
+
+    //Size of a pointer variable
+    cout<<"size of int pointer "<< sizeof(ptr)<<endl;
+    cout<<"size of float pointer "<<sizeof(aptr)<<endl;
+    cout<<"size of char pointer "<<sizeof(chptr)<<endl;
+
+    // Every pointer variable will have SAME SPACE
+
+    // Do not write char* ptr = &<integer>
+    // It happens because when char pointer is used it points to one memory box
+    // When integer pointer is used it points to the firsts box of a 4 box memory
+
+    //Dereference operator
 
     return  0;
 }
