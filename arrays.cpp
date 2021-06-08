425a926aaa9fba0a77f37ba2e3458371ec9ed645
hareshnayak <sketchharry01@gmail.com>
2021-06-08 15:41:49 +0530

    Kadane's Algorithm

--- arrays.cpp
+++ arrays.cpp
@@ -7,12 +7,17 @@ GENERATING SUBARRAYS
 
 Given an array output all sub-arrays
 
-Finding the maximum/minimum sum subArray
-- Generate all the subArrays
-- Compare with current sum to max sum
 */
 void maxSubArray(int n ,int arr[])
 {
+/*
+Finding the maximum/minimum sum subArray
+- Generate all the subArrays
+- Compare with current sum to max sum
+
+time complexity - n^3
+*/
+
 int c=0,maxSum = 0, currentSum;
 int left, right;
 
@@ -31,12 +36,58 @@ for(int i =0 ; i<n ; i++){
             maxSum = currentSum;
             left = i;
             right = j;
+            }
         }
-    }
     cout<<endl;
     }
-cout<<"max Sum : "<<maxSum<<" with subArray : "<<left<<","<<right;
+cout<<"max Sum : "<<maxSum<<" is no."<<c<<" with subArray : "<<left<<","<<right;
 }
+
+void csumMaxSubArray(int n , int arr[])
+{
+    int cs[n] = {0}, ms = 0;
+    int left, right, currentSum = 0;
+    //Making Array of cumulative sum
+    for(int i =0; i<n;i++)
+    {
+        if(i ==0)
+            cs[i]=arr[i];
+        else
+        cs[i]=cs[i-1]+arr[i];
+    }
+    // Finding CurrentSum and then comparing it with maxSum
+    for(int i = 0 ; i<n ; i++)
+    {
+        for(int j = i; j<n;j++)
+        {
+            currentSum = cs[j] - cs[i-1];
+            if(currentSum>ms)
+            {
+                ms = currentSum;
+                left = i;
+                right = j;
+            }
+        }
+    }
+     cout<<"max sum is : "<<ms<<" at "<<left<<","<<right<<endl;
+}
+
+
+void KadanesAlgo(int n , int arr[])
+{
+    int cs = 0, ms = 0;
+    // It provides the maxSum subArray in O(n)
+    // But it does not provide the subArray
+    for(int i = 0; i<n;i++)
+    {
+        cs = cs + arr[i];
+        if(cs < 0)
+            cs = 0;
+        ms = max(cs, ms);
+    }
+    cout<<"max sum is : "<<ms<<endl;
+}
+
 
 
 int main(){
@@ -46,9 +97,11 @@ cin>>n;
 int arr[n];
 for(int i = 0; i<n;i++)
     {
-    cout<<"Enter arr["<<i<<"] : ";
+    cout<<"Enter arr["<<i<<"]: ";
     cin>>arr[i];
 }
-maxSubArray(n,arr);
+//maxSubArray(n,arr);
+csumMaxSubArray(n, arr);
+//KadanesAlgo(n, arr);
 return 0;
 }
