063646f86d256daed6ff6e092ac6578eebcb7426
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2021-07-23 00:37:50 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 /*
 ARRAYS
@@ -75,15 +75,15 @@ void csumMaxSubArray(int n , int arr[])
 
 void KadanesAlgo(int n , int arr[])
 {
-    int cs = 0, ms = 0;
+    int cs = 0, ms = INT_MIN;
     // It provides the maxSum subArray in O(n)
     // But it does not provide the subArray
     for(int i = 0; i<n;i++)
     {
-        cs = cs + arr[i];
+        cs = cs + arr[i];       
+        ms = max(cs, ms);
         if(cs < 0)
             cs = 0;
-        ms = max(cs, ms);
     }
     cout<<"max sum is : "<<ms<<endl;
 }
